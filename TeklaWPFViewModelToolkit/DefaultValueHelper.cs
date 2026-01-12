6177a2136e08926c31afb8d7b1a855e1d4722ec0
Anatolii Ivanov <gotnickname@gmail.com>
2026-01-12 16:48:51 +0400

    add IsDefault(value)

---
+++ TeklaWPFViewModelToolkit/DefaultValueHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace TeklaWPFViewModelToolkit;
+
+public static class DefaultValueHelper
+{
+    public static bool IsDefaultValue(string value)
+    {
+        return value == "";
+    }
+    public static bool IsDefaultValue(int value)
+    {
+        return value == int.MinValue;
+    }
+    public static bool IsDefaultValue(double value)
+    {
+        return value == (double)int.MinValue;
+    }
+}
